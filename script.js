4d743aac10e221573edff62d40732b0363676083
MamatovaElnoza <mamatovaelnoza@gmail.com>
2024-11-28 21:44:31 +0500

    For Loop and If Else Statement on the Prompt with Series

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const numberOfSeries = prompt("Nechta serial ko’rdingiz?");
 
 const seriesDB = {
-    count: numberOfSeries,
-    series: {},
-    actors: {},
-    genres: {},
-    privat: false
-}
+  count: numberOfSeries,
+  series: {},
+  actors: {},
+  genres: {},
+  privat: false,
+};
 
-const question1 = prompt('Oxirgi ko’rgan serialingiz?');
-const rate1 = prompt('Nechi baxo berasiz?');
-const question2 = prompt('Oxirgi ko’rgan serialingiz?');
-const rate2 = prompt('Nechi baxo berasiz?');
+  const question1 = prompt("Oxirgi ko’rgan serialingiz?", "");
+  const rate1 = prompt("Nechi baxo berasiz?", "");
 
-seriesDB.series[question1] = rate1
-seriesDB.series[question2] = rate2
+  seriesDB.count++;
 
-console.log(seriesDB);
+  if (seriesDB.count <= 5) {
+    console.log('Kam serial ko’ripsiz');
+  } else if (seriesDB.count <= 10) {
+    console.log('Siz classik tamoshabin ekansiz');
+  } else if (seriesDB.count >= 15) {
+    console.log('Siz serialchi zvezda ekansiz');
+  } 
+
+//   console.log(question1);
+//   console.log(rate1);
+  seriesDB.series[question1] = rate1
+
+
+
